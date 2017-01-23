207f1f6a92274bf12caf0a79cca294f82faef778
Maurelian <leighvert@protonmail.ch>
2017-01-23 06:19:25 -0500

    Revert "debugging transfer"
    This reverts commit 44e1d6fb3f1811b8522428dda12ed352ab55a847.

--- src/test/test.js
+++ src/test/test.js
@@ -169,7 +169,6 @@ describe('Registrar', () => {
     it('Should return the correct properties of a name', (done) => {
       // a name being auctioned
       registrar.getEntry('foobarbaz', (entryErr1, entryResult1) => {
-        debugger;
         assert.equal(entryErr1, null);
         assert.equal(entryResult1.name, 'foobarbaz');
         assert.equal(entryResult1.status, 1);
@@ -180,7 +179,6 @@ describe('Registrar', () => {
         assert.equal(entryResult1.highestBid, 0);
         // a name NOT being auctioned
         registrar.getEntry('thisnameisopen', (entryErr2, entryResult2) => {
-          debugger;
           assert.equal(entryErr2, null);
           assert.equal(entryResult2.name, 'thisnameisopen');
           assert.equal(entryResult2.status, 0);
@@ -336,11 +334,8 @@ describe('Registrar', () => {
     it('Should throw an error if the sender is not the owner', (done) => {
       registrar.transfer('foobarbaz', accounts[9], { from: accounts[8], gas: 4700000 },
         (transferNameErr, transferNameResult) => {
-          debugger;
-          assert.equal(
-            transferNameErr.toString().indexOf('Only the current owner') !== -1, transferNameErr
-          );
-          assert.equal(transferNameResult, null);
+          assert.equal(transferNameErr, null);
+          assert.ok(typeof transferNameResult === 'string');
           done();
         }
       );
